a505056fdd45c02ba060800a3e6cba422a791a69
Arunpujari97 <arunkumarkh124@gmail.com>
2025-06-29 20:38:53 +0530

    Add files via upload

---
+++ searching.py
@@ -0,0 +1,45 @@
+############################# Binary searching
+# 1) the array must aling in sorted order
+
+# arr=[]
+# a=input("enter the elements to arr separated by space").split()
+# for i in a:
+#     arr.append(int(i))
+# arr=sorted(arr)
+# key_value=int(input("enter the element to search"))
+#
+# first=0
+# last=len(arr)+1
+# while first<=last:
+#     mid=(first+last)//2
+#     if key_value==arr[mid]:
+#         print("key",key_value,"found at",mid)
+#         break
+#     elif key_value < arr[mid]:
+#         last=mid-1
+#     else:
+#         first=mid+1
+# else:
+#     print("element not found")
+
+
+
+
+################################# sequential search
+# arr=[]
+# a=input("enter the elemnets to array").split()
+# for i in a:
+#     arr.append(i)
+# key_value=input("enter the element to search")
+# first=0
+# last=len(arr)+1
+# while first<=last:
+#     if arr[first]==key_value:
+#         print("the element is ",key_value,"found at",first)
+#         break
+#     else:
+#         first+=1
+# else:
+#     print("element is not found")
+
+
